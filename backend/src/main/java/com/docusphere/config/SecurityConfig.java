c242024426d74d7d4538a2fd4f2e98cf12b2d448
Lalegende21 <delfredtene17@gmail.com>
2026-09-23 12:25:30 +0100

    feat(backend): add cors configuration

--- backend/src/main/java/com/docusphere/config/SecurityConfig.java
+++ backend/src/main/java/com/docusphere/config/SecurityConfig.java
@@ -46,6 +46,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/auth/**").permitAll()
